d48eee8907ddfd26bed1890eee550d73542ffcaa
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-12-14 11:38:48 -0800

    net: netlink: rename NETLINK_DUMP_STRICT_CHK -> NETLINK_GET_STRICT_CHK
    NETLINK_DUMP_STRICT_CHK can be used for all GET requests,
    dumps as well as doit handlers.  Replace the DUMP in the
    name with GET make that clearer.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: David Ahern <dsahern@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- tools/include/uapi/linux/netlink.h
+++ tools/include/uapi/linux/netlink.h
@@ -29,12 +29,8 @@
 #define NETLINK_RDMA		20
 #define NETLINK_CRYPTO		21	/* Crypto layer */
 #define NETLINK_SMC		22	/* SMC monitoring */
-#define NETLINK_SOCKEV		23	/* Socket Administrative Events */
+
 #define NETLINK_INET_DIAG	NETLINK_SOCK_DIAG
-#define NETLINK_ADB         23
-/* WIFI MODIFICATION */
-#define NETLINK_OP_SLA         29      /* SLA NETLINK SOCK */
-/* WIFI MODIFICATION */
 
 #define MAX_LINKS 32		
 
